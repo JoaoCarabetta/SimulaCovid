5e23e52f7ceb945a4119b84efd1eff106a3c8045
Fernanda Scovino <fscovinom@gmail.com>
2020-03-21 18:53:15 -0300

    add clean bases script + fix raw script

---
+++ scripts/clean_databases.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from paths import RAW_PATH, TREAT_PATH
+from datetime import datetime as dt
+from unidecode import unidecode
+
+def join_city_state(city, state):
+    """
+    Join and clean city + state names to cross databases.
+    """
+    
+    if city:
+        city_state = unidecode(city.upper()) + ' ' + state
+        return city_state
+    
+    return ''
+
+def treat(df, filename):
+    """
+    Treat single database for analysis. 
+    """
+    
+    # Add city + state column to cross
+    df['municipio'] = df['municipio'].fillna('')
+    df['municipio_uf'] = df.apply(lambda row: join_city_state(row['municipio'], row['uf']), axis=1)
+    
+    # Save clened database
+    treat_name = 'treated_'+filename
+    df.to_csv(TREAT_PATH / treat_name)
+    
+    return df
+
+
+def treat_all():
+    """
+    Treat all databases for analysis.
+    """
+    
+    raw_files = sorted(RAW_PATH.glob("*.csv"))
+    
+    dfs = []
+    for filepath in raw_files:
+
+        df = pd.read_csv(filepath)
+        filename = filepath.parts[-1]
+
+        dfs.append(treat(df, filename))
+        
+    return dfs
+    
+
+if __name__ == '__main__':
+    treat_all()
--- scripts/get_raw.py
+++ scripts/get_raw.py
@@ -6,7 +6,7 @@ import pandas as pd
 from paths import RAW_PATH
 from datetime import datetime as dt
 
-def get_cases():
+def covid_cases():
     """
     Collect & save COVID-19 cases dataset
     """
@@ -22,11 +22,16 @@ def get_cases():
     # Get current date & time
     today = str(dt.today())
     df['last_update'] = today
+    
+    # Change to pt-br
+    df = df.rename({'city': 'municipio', 'state': 'uf'}, axis=1)
+    df['municipio'] = df['municipio'].fillna('')
 
     # Save/update raw dataset
-    df.to_csv(RAW_PATH / 'covid19_cases.csv')
+    df.to_csv(RAW_PATH / 'covid19_cases.csv', index=False)
     
     return df
 
+    
 if __name__ == '__main__':
     get_cases()
