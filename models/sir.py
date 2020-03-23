4eb349b8d9be89aadf213f0cf87f3a2c23ce5b8a
Fernanda Scovino <fscovinom@gmail.com>
2020-03-23 12:50:48 -0300

    add days column on output

--- models/sir.py
+++ models/sir.py
@@ -19,7 +19,7 @@ def entrypoint(current_state, model_parameters):
                current_state['current_infected'],                                   # I 
                0                                                                    # R         
               ),
-        't': np.linspace(0, model_parameters['days_from_t0'], model_parameters['days_from_t0']),
+        't': np.linspace(0, model_parameters['days_from_t0'], model_parameters['days_from_t0']+1),
         'args': (current_state['population'],                            # N
                  model_parameters['R0'] / model_parameters['sick_days'], # beta
                  1. / model_parameters['sick_days'],                     # gamma
@@ -30,6 +30,7 @@ def entrypoint(current_state, model_parameters):
     
     result = pd.DataFrame(result, columns=['S', 'R', 'I'])
     
+    result['days'] = args['t']
     result['I2'] = result['I'] * model_parameters['i2_percentage'] / 100 # severe cases
     result['I3'] = result['I'] * model_parameters['i3_percentage'] / 100 # critical cases
     result['I1'] = result['I'] - result['I2'] - result['I3']             # mild cases
